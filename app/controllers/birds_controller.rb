fcfbbbecb18470c4b01e75c37f12d6c1de17a7a0
Ronald Barasa <barasaogoro@gmail.com>
2023-06-24 23:19:01 +0300

    complete assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -37,6 +38,10 @@ class BirdsController < ApplicationController
 
   def find_bird
     Bird.find(params[:id])
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   def bird_params
